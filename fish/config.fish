0f351e5f8d7ba759a1ab5b288802d3434230e900
asyraf07 <asyraf.sr07@gmail.com>
2021-11-03 00:08:12 +0700

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,35 @@
+set fish_greeting ""
+
+set -g theme_use_abbreviated_branch_name yes
+set -g theme_display_vagrant yes
+set -g theme_display_docker_machine no
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv no
+set -g theme_display_nix no
+set -g theme_display_ruby no
+set -g theme_display_node yes
+set -g theme_display_user ssh
+set -g theme_display_hostname ssh
+set -g theme_display_vi no
+set -g theme_display_date no
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_title_display_path no
+set -g theme_title_display_user yes
+set -g theme_title_use_abbreviated_path yes
+set -g theme_date_format "+%a %H:%M"
+set -g theme_date_timezone Indonesia/Jakarta
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts no
+set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose yes
+set -g default_user your_normal_user
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_project_dir_length 1
+
+alias ll "exa -l -g --icons"
+alias lla "ll -a"
+
